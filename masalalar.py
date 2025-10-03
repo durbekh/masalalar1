9c5d63614858c3d2b9faea57a1d8cb7ea34cc521
Durbek Hayotbekov <d83217311@gmail.com>
2025-10-03 15:29:25 +0500

    Update masalalar.py
    kommentga oldim

--- masalalar.py
+++ masalalar.py
@@ -2,8 +2,9 @@
 # b = int(input("Ikkinchi sonni kiriting : "))
 # print(f"yig'indi {a+b}")
 
-ism = input("Ismingizni kiriting: ").capitalize()
-familya = input("Familya kiriting: ").capitalize()
-otasining_i = input("Otangizning ismini kiriting: ").capitalize()
-print(f"{ism} \n {familya} \n {otasining_i}")
+# ism = input("Ismingizni kiriting: ").capitalize()
+# familya = input("Familya kiriting: ").capitalize()
+# otasining_i = input("Otangizning ismini kiriting: ").capitalize()
+# print(f"{ism} \n {familya} \n {otasining_i}")
+
 
