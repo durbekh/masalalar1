e7c4988e6ffa7a6f100f64d325018e775eaccb61
Durbek Hayotbekov <d83217311@gmail.com>
2025-10-03 16:14:04 +0500

    Add files via upload

---
+++ arfimetik_operatorlar.py
@@ -0,0 +1,49 @@
+# # 1
+# a = int(input("Birinchi sonni kiriting:"))
+# b = int(input("Ikkinchi sonni kiriting:"))
+# print(f"Yig'indi : {a+b}")
+# # 2
+# l = int(input("Uzunligini kiriting:"))
+# w = int(input("Enini kiriting:"))
+# area = l*w
+# print(f"Yuzi: {area}")
+# # 3
+# x = int(input("Birinchi sonni kiriting:"))
+# y = int(input("Ikkinchi sonni kiriting:"))
+# z = int(input("Uchinchi sonni kiriting:"))
+# avg = (x+y+z) / 3
+# print(f"O'rta arfimetik: {avg}")
+# # 4
+# a = int(input("Birinchi tomonini kiriting:"))
+# b = int(input("Ikkinchi tomonini kiriting:"))
+# p = 2 * (a + b)
+# print(f"Perimetri: {p}")
+# # 5
+# h = int(input("Nechcha soat:"))
+# m = int(input("Nechcha daqiqa:"))
+# total_min=h * 60 + m
+# print(f"{total_min} daqiqa")
+# # 6
+# d = int(input("Qancha dolloringiz borligini kiriting: "))
+# som = d * 12000
+# print(f"{som} so'mi")
+# # 7
+# xy = int(input("Birinchi sonni kiriting:"))
+# yx= int(input("Ikkinchi sonni kiriting:"))
+# diff = abs(xy - yx)
+# print(f"Modil farqi : {diff}")
+# # 8
+# ad = int(input("Qaysi sonni darajaga ko'tarasiz :"))
+# ba = int(input("Qaysi darajaga ko'tarasiz :"))
+# power = ad ** ba
+# print(f" {ad} ning {ba}- darajasi :{power}")
+# # 9
+# adc = int(input("Birinchi sonni kiriting:"))
+# bac = int(input("Ikkinchi sonni kiriting :"))
+# quotient = adc // bac
+# print(f"Qoldiq : {quotient}")
+# # 10
+# abd = int(input("Almashtirmoqchi bo'lgan soningizni kiriting:"))
+# abc = int(input("Almashtirilishi kerak bo'lgan sonni kitriting  :"))
+# abd, abc = abc, abd
+# print(f"Natija : {abd , abc}")
