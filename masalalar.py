3dd50ac6a607df3099a8f05cb60728d0d2e87cf1
Durbek Hayotbekov <d83217311@gmail.com>
2025-10-03 15:28:42 +0500

    Add files via upload

---
+++ masalalar.py
@@ -0,0 +1,9 @@
+# a = int(input("Birinchi sonni kiriting : "))
+# b = int(input("Ikkinchi sonni kiriting : "))
+# print(f"yig'indi {a+b}")
+
+ism = input("Ismingizni kiriting: ").capitalize()
+familya = input("Familya kiriting: ").capitalize()
+otasining_i = input("Otangizning ismini kiriting: ").capitalize()
+print(f"{ism} \n {familya} \n {otasining_i}")
+
