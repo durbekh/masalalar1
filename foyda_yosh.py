0aa308ae9f92c52cdd2f9f0d67b6cb69f5a08bd8
Durbek Hayotbekov <d83217311@gmail.com>
2025-10-17 14:59:41 +0500

    Add files via upload

---
+++ foyda_yosh.py
@@ -0,0 +1,52 @@
+
+j = input("Jinsingizni kiriting (erkak yoki ayol): ").lower()
+y = int(input("yoshingizni kiriting : "))
+if 0 <= y <= 120 :
+    if j == "erkak" and y >= 60:
+        n_y = y - 60
+        if n_y == 0 :
+            print(f"Bobo siz nafaqa chiqgansiz . \nNafaqaga shu yili chiqgansiz")
+        else:
+            print(f"Bobo siz nafaqa yoshidasiz . \nNafaqaga cgiqganingizga {n_y} yil bo'ldi")
+    elif j == "erkak" and y < 60 :
+        n_ye = 60 - y
+        if n_ye == 0 :
+            print(f"Hurmatli foydalanuvchi siz hali nafaqaga chiqmagansiz . \nNafaqaga yaqinda chiqdingiz . ")
+        else:
+            print(f"Hurmatli foydalanuvchi siz hali nafaqaga chiqmagansiz . \nNafaqaga chiqishingizga {n_ye} yil bor .")
+    elif j == "ayol" and y < 60 :
+        n_yea = y - 60
+        if n_yea == 0 :
+            print(f"Hurmatli foydalanuvchi siz hali nafaqaga chiqmagansiz .\nNafaqaga yaqinda chiqdingiz . ")
+        else:
+            print(f"Hurmatli foydalanuvchi siz hali nafaqaga chiqmagansiz . \nNafaqaga chiqishingizga {n_yea} yil bor .")
+    elif j == "ayol" and y >= 60 :
+        n_yy = y - 60
+        if n_yy == 0 :
+            print(f"Buvi siz nafaqa yoshidasiz . \nNafaqaga yaqinda chiqdingiz")
+        else:
+            print(f"Buvi siz nafaqa yoshidasiz .\nNafaqaga cgiqganingizga {n_yy} yil bo'ldi")
+    else:
+        print("Hurmatli foydalanuvchi siz jinsingizni to'g'ri kiritishingiz kerak")
+else:
+    print("Yoshingizni to'g'ri kiriting ! ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
