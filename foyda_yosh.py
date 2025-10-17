b3c49d5346c79f43e67de9664126137ee0da857e
Durbek Hayotbekov <d83217311@gmail.com>
2025-10-17 15:49:16 +0500

    Update foyda_yosh.py
    kommet ichidagi so'zlarning hatosi to'g'irlandi

--- foyda_yosh.py
+++ foyda_yosh.py
@@ -6,7 +6,7 @@ if 0 <= y <= 120 :
         if n_y == 0 :
             print(f"Bobo siz nafaqa chiqgansiz . \nNafaqaga shu yili chiqgansiz")
         else:
-            print(f"Bobo siz nafaqa yoshidasiz . \nNafaqaga cgiqganingizga {n_y} yil bo'ldi")
+            print(f"Bobo siz nafaqa yoshidasiz . \nNafaqaga chiqganingizga {n_y} yil bo'ldi")
     elif j == "erkak" and y < 60 :
         n_ye = 60 - y
         print(f"Hurmatli foydalanuvchi siz hali nafaqaga chiqmagansiz . \nNafaqaga chiqishingizga {n_ye} yil bor .")
@@ -18,11 +18,12 @@ if 0 <= y <= 120 :
         if n_yy == 0 :
             print(f"Buvi siz nafaqa yoshidasiz . \nNafaqaga yaqinda chiqdingiz")
         else:
-            print(f"Buvi siz nafaqa yoshidasiz .\nNafaqaga cgiqganingizga {n_yy} yil bo'ldi")
+            print(f"Buvi siz nafaqa yoshidasiz .\nNafaqaga chgiqganingizga {n_yy} yil bo'ldi")
     else:
         print("Hurmatli foydalanuvchi siz jinsingizni to'g'ri kiritishingiz kerak")
 else:
     print("Yoshingizni to'g'ri kiriting ! ")
+
 
 
 
