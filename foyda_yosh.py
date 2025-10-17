fa20a52d6d048cd87d21b2db16dbf9e464bb0ab3
Durbek Hayotbekov <d83217311@gmail.com>
2025-10-17 16:03:34 +0500

    Update foyda_yosh.py

--- foyda_yosh.py
+++ foyda_yosh.py
@@ -10,11 +10,11 @@ if 0 <= y <= 120 :
     elif j == "erkak" and y < 60 :
         n_ye = 60 - y
         print(f"Hurmatli foydalanuvchi siz hali nafaqaga chiqmagansiz . \nNafaqaga chiqishingizga {n_ye} yil bor .")
-    elif j == "ayol" and y < 60 :
-        n_yea = y - 60
+    elif j == "ayol" and y < 55 :
+        n_yea = y - 55
         print(f"Hurmatli foydalanuvchi siz hali nafaqaga chiqmagansiz . \nNafaqaga chiqishingizga {n_yea} yil bor .")
-    elif j == "ayol" and y >= 60 :
-        n_yy = y - 60
+    elif j == "ayol" and y >= 55 :
+        n_yy = y - 55
         if n_yy == 0 :
             print(f"Buvi siz nafaqa yoshidasiz . \nNafaqaga yaqinda chiqdingiz")
         else:
@@ -23,6 +23,7 @@ if 0 <= y <= 120 :
         print("Hurmatli foydalanuvchi siz jinsingizni to'g'ri kiritishingiz kerak")
 else:
     print("Yoshingizni to'g'ri kiriting ! ")
+
 
 
 
